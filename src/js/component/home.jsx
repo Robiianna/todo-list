4a4269c72f80ff8ecf4b7f64645f38f8064c9aa6
Robiianna <robidelaparra1@gmail.com>
2022-06-13 14:28:11 +0000

    listo todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,20 +2,65 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	//creamos 2 estados, task para guardar lo que hace el usuario y todo para la lista de tareas
 	const [task, setTask] = useState("");
 	const [todos, setTodos] = useState([]);
+	//aplicamos funciones con evento change que detecta cambios en el input y los agrega en el task
+	function handleChange(event) {
+		setTask(event.target.value);
+	}
+	//una vez terminado el evento onchange se agrega en el edo todo
+	function addTask(event) {
+		if (event.key === "Enter") {
+			if (task.trim() !== "") {
+				setTodos([...todos, task]);
+				setTask("");
+			}
+		}
+	}
+	//una esta funcion elimia la tarea, recibe el id como parametro (index) eso no se suele usar para eliminar, solo de usa para entrenar
+	function deleteTask(id) {
+		let newTask = todos.filter((ta, index) => id !== index);
+		setTodos(newTask);
+	}
 	return (
 		<div className="container">
 			<div className="row justify-content-center">
 				<div className="col-12 col-md-6">
 					<h1 className="text-center"> My ToDo List</h1>
-					<form>
+					<form
+						onSubmit={(event) => {
+							event.preventDefault();
+						}}>
 						<input
 							type="text"
 							placeholder="add your task"
 							className="form-control"
+							value={task}
+							onChange={handleChange}
+							onKeyDown={addTask}
+							//eventos
 						/>
 					</form>
+					{todos.length > 0 ? (
+						<p> `te faltan {todos.length} tareas`</p>
+					) : (
+						<p>congratulation your ending tasks</p>
+					)}
+					{/* //operador */}
+					<ul>
+						{todos.map((ta, index) => {
+							return (
+								<li
+									key={index}
+									onClick={() => {
+										deleteTask(index);
+									}}>
+									{ta}
+								</li>
+							);
+						})}
+					</ul>
 				</div>
 			</div>
 		</div>
